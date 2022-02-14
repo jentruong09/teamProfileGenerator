c78ec1db3e7e7a6b4fe30d79804020c15937e720
Jen Truong <jennifertruong09@gmail.com>
2022-02-14 09:33:03 -0500

    added a simple layout of code that i will need to write

---
+++ index.js
@@ -0,0 +1,47 @@
+// Node Modules
+const fs = require('fs')
+const inquirer = require('inquirer')
+
+// To link to generate HTML js
+const createHTML = require('./src/createHTML')
+
+// Array for all the answers for the team - for all employees
+const arrayTeamAnswer = []
+
+// Questions for Manager
+const managerInquiry = () => {
+    inquirer
+        .prompt([
+            {
+                type: ,
+                message: ,
+                name:
+            },
+        ])
+}
+
+
+// Questions for Engineer
+const engineerInquiry = () => {
+    inquirer
+        .prompt([
+            {
+                type: ,
+                message: ,
+                name:
+            },
+        ])
+}
+
+
+// Question for Intern
+const internInquiry = () => {
+    inquirer
+        .prompt([
+            {
+                type: ,
+                message: ,
+                name:
+            },
+        ])
+}
