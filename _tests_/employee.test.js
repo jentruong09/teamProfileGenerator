a8c7681abd85a4086babeb3f382cd61dea1fe7f2
Jen Truong <jennifertruong09@gmail.com>
2022-02-14 20:41:59 -0500

    created classes for manager and intern + created test for employee

--- _tests_/employee.test.js
+++ _tests_/employee.test.js
@@ -1,15 +1,44 @@
 // constructor
-const { expect } = require('@jest/globals') // this auto-populated?
-const { it } = require('jest-circus') // this auto-populated?
+// const { expect } = require('@jest/globals') // this auto-populated?
+// const { it } = require('jest-circus'); // this auto-populated?
+// const { describe } = require('yargs'); // this auto-populated?
 const Employee = require('../lib/employee')
 
 describe('Employee', () => {
     it('correct input for employee object', () => {
-        const employee = new Employee('name','id','email') //change this to be an example of each? 
+        const employee = new Employee('jen','100','email@email.com') //change this to be an example of each? 
 
-        expect(employee.name).toBe('string') //to let the test expect a string in return for the name
-        expect(employee.id).toBe('number') // to let the test expect number(s) in return for the id
-        expect(employee.email).toBe('string') //to let the test expext a string in return for the email -- not sure how it will know it is an email? 
+        expect(employee.name).toBe('jen') //to let the test expect a string in return for the name
+        expect(employee.id).toBe('100') // to let the test expect number(s) in return for the id
+        expect(employee.email).toBe('email@email.com') //to let the test expext a string in return for the email -- not sure how it will know it is an email? 
+    });
+
+    describe('getName', () => {
+        it('getting the employee name from getName()', () => {
+            const employee = new Employee('jen','100','email@email.com') 
+            expect(employee.getName()).toBe('jen')
+        })
+    });
+
+    describe('getId', () => {
+        it('getting the employee id number from getId()', () => {
+            const employee = new Employee('jen','100','email@email.com') 
+            expect(employee.getId()).toBe('100')
+        })
+    });
+
+    describe('getEmail', () => {
+        it('getting the employee email address from getEmail()', () => {
+            const employee = new Employee('jen','100','email@email.com') 
+            expect(employee.getEmail()).toBe('email@email.com')
+        })
+    });
+
+    describe('getRole', () => {
+        it('getting the employee role from getRole()', () => {
+            const employee = new Employee('jen','100','email@email.com') 
+            expect(employee.getRole()).toBe('Employee')
+        })
     })
 })
 
